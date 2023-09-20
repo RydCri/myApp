821958b7279f0917a1a0419fdd6d09391fddf3e9
RydCri <107228115+RydCri@users.noreply.github.com>
2023-09-20 18:04:31 -0500

    express generate's boilerplate throws 500 error on purpose, fixed
    -added 404 handler and res.render

--- app.js
+++ app.js
@@ -22,35 +22,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.get('/', function(req, res) {
-  res.render('views/index');
-});
+// app.get('/', function(req, res) {
+//   res.render('views/index');
+// });
+
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 app.set('port', PORT);
 app.set('env', NODE_ENV);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+    const err = new Error(`${req.method} ${req.url} Not Found`);
+    err.status = 404;
+    //404 view
+    res.render('404')
+    next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message,
+        },
+    });
 });
+
 app.listen(PORT, () => {
-  console.log(
-      `Express Server started on Port ${app.get(
-          'port'
-      )} | Environment : ${app.get('env')}`
-  );
+    console.log(
+        `Express Server started on Port ${app.get(
+            'port'
+        )} | Environment : ${app.get('env')}`
+    );
 });
 
 module.exports = app;
