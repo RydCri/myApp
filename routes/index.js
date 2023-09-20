d19a89d11c1c13dc70273b91f92cda4f20158ac7
RydCri <107228115+RydCri@users.noreply.github.com>
2023-09-20 18:05:04 -0500

    simplified router

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var router = express.Router();
-// let ejs = require('ejs');
-// let people = ['geddy', 'neil', 'alex'];
-// let html = ejs.render('<%= people.join(", "); %>', {people: people});
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('views/index');
+  res.render('index');
 });
 
 module.exports = router;
