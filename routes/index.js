5b616731f8ec22cbe44305dec1e9698ac04653b6
RydCri <107228115+RydCri@users.noreply.github.com>
2023-09-19 20:37:38 -0500

    replaced jade w/ ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+// let ejs = require('ejs');
+// let people = ['geddy', 'neil', 'alex'];
+// let html = ejs.render('<%= people.join(", "); %>', {people: people});
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('views/index');
 });
 
 module.exports = router;
