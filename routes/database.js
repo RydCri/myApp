4734c0cd1526532bc147533609e50f028ef09c76
RydCri <107228115+RydCri@users.noreply.github.com>
2023-09-21 11:04:53 -0500

    fetch options updated

--- routes/database.js
+++ routes/database.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-let options =  {
-    headers: {
-        'Accept': 'application/json'
-    },
-    method: 'GET'
-};
 const fetchResource = () => {
-    let response = fetch('/public/data/dummyDB.json', options);
+    let response;
+    let options =  {
+        headers: {
+            'Accept': 'application/json'
+        },
+        method: 'GET',
+        body: JSON.stringify(response)
+        };
+    response = fetch('/public/data/dummyDB.json', options);
     return (response);
 }
 router.get('/', async (req, res) => {
