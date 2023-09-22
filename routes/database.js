df9db0b24846820e6f6bef4516c264d91cb2a132
RydCri <107228115+RydCri@users.noreply.github.com>
2023-09-21 21:27:35 -0500

    database now returns the JSON object, not a body render

--- routes/database.js
+++ routes/database.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var resource = require('../public/data/dummyDB.json')
 
 router.get('/', async (req, res) => {
-    res.send(`${(JSON.stringify(resource))}`);
+    res.send(resource)
+    console.log(resource);
 });
 
 module.exports = router;
